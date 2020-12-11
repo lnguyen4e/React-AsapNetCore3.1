209dfaf1a57a09b8cdbe1f50a19c6d841f822403
Lam Nguyen <l0nguy10@louisville.edu>
2020-12-11 15:05:14 -0500

    video 199

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace Application.Activities
 {
-   public class ActivityDto
+    public class ActivityDto
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
@@ -13,6 +14,7 @@ namespace Application.Activities
         public DateTime Date { get; set; }
         public string City { get; set; }
         public string Venue { get; set; }
+        [JsonPropertyName("Attendees")]
         public ICollection<AttendeeDto> UserActivities { get; set; }
     }
 }
