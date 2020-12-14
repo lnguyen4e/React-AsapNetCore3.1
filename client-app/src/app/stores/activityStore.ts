dc69874fdc141e0ca8f06a7a3ef00efebb0b7a04
Lam Nguyen <l0nguy10@louisville.edu>
2020-12-13 21:58:45 -0500

    video 213

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -5,6 +5,7 @@ import agent from '../api/agent';
 import { history } from '../..';
 import { toast } from 'react-toastify';
 import { RootStore } from './rootStore';
+import { createAttendee, setActivityProps } from '../common/util/util';
 
 
 
@@ -20,7 +21,7 @@ export default class ActivityStore {
   @observable loadingInitial = false;
   @observable submitting = false;
   @observable target = '';
-
+  @observable loading = false;
   @computed get activitiesByDate() {
     return this.groupActivitiesByDate(Array.from(this.activityRegistry.values()))
   }
@@ -38,11 +39,12 @@ export default class ActivityStore {
 
   @action loadActivities = async () => {
     this.loadingInitial = true;
+    
     try {
       const activities = await agent.Activities.list();
       runInAction('loading activities', () => {
         activities.forEach(activity => {
-          activity.date = new Date(activity.date);
+          setActivityProps(activity,this.rootStore.userStore.user!);
           this.activityRegistry.set(activity.id, activity);
         });
         this.loadingInitial = false;
@@ -140,6 +142,52 @@ export default class ActivityStore {
       })
       console.log(error);
     }
+  }
+  @action attendActivity =async () =>{
+    const attendee = createAttendee(this.rootStore.userStore.user!);
+    this.loading=true;
+    try{
+      await agent.Activities.attend(this.activity!.id);
+      runInAction(() =>{
+        if(this.activity) {
+          this.activity.attendees.push(attendee);
+          this.activity.isGoing = true;
+          this.activityRegistry.set(this.activity.id,this.activity);
+          this.loading = false;
+        }
+      })
+    }
+    catch(error)
+    {
+      runInAction(()=>{
+        this.loading= false;
+      })
+     
+      toast.error('Problem signing up to activity');
+    }
+  
+  }
+  @action cancelAttendance =async () =>{
+    this.loading= true;
+    try
+    {
+     await agent.Activities.unattend(this.activity!.id);
+     runInAction(()=>{
+      if(this.activity) {
+        this.activity.attendees = this.activity.attendees.filter(a=>a.username!== this.rootStore.userStore.user!.username);
+        this.activity.isGoing =false;
+        this.activityRegistry.set(this.activity.id,this.activity);
+        this.loading= false;
+      }
+     })
+    }
+    catch(error){
+      runInAction(()=>{
+        this.loading=false;
+      })
+      toast.error('Problem cancelling attendance');
+    }
+   
   }
 }
 
--- client-app/src/features/activities/details/ActivityDetailedSidebar.tsx
+++ client-app/src/features/activities/details/ActivityDetailedSidebar.tsx
@@ -1,8 +1,13 @@
 import React, { Fragment } from 'react';
 import { Segment, List, Item, Label, Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
-
-const ActivityDetailedSidebar = () => {
+import { IAttendee } from '../../../app/models/activity';
+import { observer } from 'mobx-react-lite';
+interface IProps{
+  attendees:IAttendee[]
+}
+const ActivityDetailedSidebar:React.FC<IProps> = ({attendees}) => {
+  const isHost=false;
   return (
     <Fragment>
       <Segment
@@ -13,49 +18,33 @@ const ActivityDetailedSidebar = () => {
         inverted
         color='teal'
       >
-        3 People Going
+        {attendees.length} {attendees.length === 1? 'Person' :'People'} going
       </Segment>
       <Segment attached>
         <List relaxed divided>
-          <Item style={{ position: 'relative' }}>
+          {attendees.map((attendee)=>(
+            <Item key={attendee.username} style={{ position: 'relative' }}>
+              {isHost &&
             <Label
               style={{ position: 'absolute' }}
               color='orange'
               ribbon='right'
             >
               Host
-            </Label>
-            <Image size='tiny' src={'/assets/user.png'} />
+            </Label>}
+            <Image size='tiny' src={attendee.image || '/assets/user.png'} />
             <Item.Content verticalAlign='middle'>
               <Item.Header as='h3'>
-                <Link to={`#`}>Bob</Link>
+                <Link to={`/profile/${attendee.username}`}>{attendee.displayName}</Link>
               </Item.Header>
               <Item.Extra style={{ color: 'orange' }}>Following</Item.Extra>
             </Item.Content>
           </Item>
-
-          <Item style={{ position: 'relative' }}>
-            <Image size='tiny' src={'/assets/user.png'} />
-            <Item.Content verticalAlign='middle'>
-              <Item.Header as='h3'>
-                <Link to={`#`}>Tom</Link>
-              </Item.Header>
-              <Item.Extra style={{ color: 'orange' }}>Following</Item.Extra>
-            </Item.Content>
-          </Item>
-
-          <Item style={{ position: 'relative' }}>
-            <Image size='tiny' src={'/assets/user.png'} />
-            <Item.Content verticalAlign='middle'>
-              <Item.Header as='h3'>
-                <Link to={`#`}>Sally</Link>
-              </Item.Header>
-            </Item.Content>
-          </Item>
+          ))}
         </List>
       </Segment>
     </Fragment>
   );
 };
 
-export default ActivityDetailedSidebar;
+export default observer(ActivityDetailedSidebar);
