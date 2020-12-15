ac33c97e4f2772e4c8dd555c5f81ce08b817ff4f
Lam Nguyen <l0nguy10@louisville.edu>
2020-12-14 20:12:07 -0500

    video 224

--- Infrastructure/Photos/PhotoAccessor.cs
+++ Infrastructure/Photos/PhotoAccessor.cs
@@ -32,10 +32,15 @@ namespace Infrastructure.Photos
                 {
                     var uploadParams = new ImageUploadParams
                     {
-                        File = new FileDescription(file.FileName, stream)
+                        File = new FileDescription(file.FileName, stream),
+                        Transformation = new Transformation().Height(500).Width(500).Crop("fill").Gravity("face")
                     };
                     uploadResult = _cloudinary.Upload(uploadParams);
                 }
+            }
+            if(uploadResult.Error != null)
+            {
+                throw new Exception(uploadResult.Error.Message);
             }
             return new PhotoUploadResult
             {
@@ -46,7 +51,9 @@ namespace Infrastructure.Photos
 
         public string Deletephoto(string publicId)
         {
-            throw new NotImplementedException();
+            var deleteParams = new DeletionParams(publicId);
+            var result = _cloudinary.Destroy(deleteParams);
+            return result.Result == "ok" ? result.Result : null;
         }
     }
 }
