5502d4708534fc522e1d0f8311e176cb611704cb
Lam Nguyen <l0nguy10@louisville.edu>
2020-12-16 18:13:55 -0500

    video 246

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -97,18 +97,17 @@ export default class ProfileStore {
             })
         }
     }
-    @action updateProfile = async (profile:Partial<IProfile>)=>{
-        try{
+    @action updateProfile = async (profile: Partial<IProfile>) => {
+        try {
             await agent.Profiles.updateProfile(profile);
-            runInAction(()=>{
-                if(profile.displayName !== this.rootStore.userStore.user!.displayName){
+            runInAction(() => {
+                if (profile.displayName !== this.rootStore.userStore.user!.displayName) {
                     this.rootStore.userStore.user!.displayName = profile.displayName!;
                 }
-                this.profile={...this.profile!,...profile};
+                this.profile = {...this.profile!, ...profile}
             })
-        }
-        catch(error){
-            toast.error('Problem updating profile');
+        } catch (error) {
+            toast.error('Problem updating profile')
         }
     }
 }
--- client-app/src/features/profiles/ProfileDescription.tsx
+++ client-app/src/features/profiles/ProfileDescription.tsx
@@ -3,19 +3,11 @@ import { Tab, Header, Card, Image, Button, Grid } from 'semantic-ui-react';
 import { RootStoreContext } from '../../app/stores/rootStore';
 import PhotoUploadWidget from '../../app/common/photoUpload/PhotoUploadWidget';
 import { observer } from 'mobx-react-lite';
-
+import ProfileEditForm from './ProfileEditForm';
 const ProfileDescription = () => {
-    const rootStore = useContext(RootStoreContext);
-    const {
-      profile,
-      isCurrentUser,
-      uploadPhoto,
-      uploadingPhoto,
-      setMainPhoto,
-      deletePhoto,
-      loading
-    } = rootStore.profileStore;
-    const [EditMode, setEditMode] = useState(false);
+  const rootStore = useContext(RootStoreContext);
+  const { updateProfile, profile, isCurrentUser } = rootStore.profileStore;
+  const [editMode, setEditMode] = useState(false);
     return (
         <Tab.Pane>
       <Grid>
@@ -23,56 +15,23 @@ const ProfileDescription = () => {
           <Header floated='left' icon='image' content='Photos' />
           {isCurrentUser && (
             <Button
-              onClick={() => setEditMode(!EditMode)}
+              onClick={() => setEditMode(!editMode)}
               floated='right'
               basic
-              content={EditMode ? 'Cancel' : 'Edit Profile'}
+              content={editMode ? 'Cancel' : 'Edit Profile'}
             />
           )}
         </Grid.Column>
         <Grid.Column width={16}>
-          {EditMode ? (
-            <PhotoUploadWidget
-              uploadPhoto={handleUploadImage}
-              loading={uploadingPhoto}
+          {editMode ? (
+            <ProfileEditForm
+            updateProfile={updateProfile}
+            profile={profile!}
             />
           ) : (
-            <Card.Group itemsPerRow={5}>
-              {profile &&
-                profile.photos.map(photo => (
-                  <Card key={photo.id}>
-                    <Image src={photo.url} />
-                    {isCurrentUser && (
-                      <Button.Group fluid widths={2}>
-                        <Button
-                          onClick={e => {
-                            setMainPhoto(photo)
-                            setTarget(e.currentTarget.name);
-                          }}
-                          name={photo.id}
-                          disabled={photo.isMain}
-                          loading={loading && target === photo.id}
-                          basic
-                          positive
-                          content='Main'
-                        />
-                        <Button
-                          name={photo.id}
-                          disabled={photo.isMain}
-                          onClick={(e) => {
-                            deletePhoto(photo)
-                            setDeleteTarget(e.currentTarget.name)
-                          }}
-                          loading={loading && deleteTarget === photo.id}
-                          basic
-                          negative
-                          icon='trash'
-                        />
-                      </Button.Group>
-                    )}
-                  </Card>
-                ))}
-            </Card.Group>
+           
+              <span>{profile!.bio}</span>
+           
           )}
         </Grid.Column>
       </Grid>
@@ -80,4 +39,4 @@ const ProfileDescription = () => {
     )
 }
 
-export default ProfileDescription
+export default observer(ProfileDescription);
